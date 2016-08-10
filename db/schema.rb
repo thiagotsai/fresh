f6fdbce3bce216b49219c44c32484fd773f693a3
caroline assis <carolinemassis@gmail.com>
2016-08-10 13:22:28 +0100

    small adjustments on the home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810101254) do
+ActiveRecord::Schema.define(version: 20160810103431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160810101254) do
     t.datetime "updated_at",   null: false
     t.string   "city"
     t.string   "country"
+    t.string   "description"
   end
 
   create_table "cuisines", force: :cascade do |t|
