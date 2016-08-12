6a50f0215d0b1ac247c12a9e26091d32f12f9207
Thiago Tsai <thiago.tsai@gmail.com>
2016-08-12 14:08:33 +0100

    Item model corrected

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :business_place
   belongs_to :user
-  has_many :item_info
+  has_many :item_info, dependent: :destroy
   has_many :ingredients, through: :item_info
   mount_uploader :photo, PhotoUploader
   accepts_nested_attributes_for :ingredients
