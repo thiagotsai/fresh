db008aa4344545a8f098558ef20ae7ad600ce962
Thiago Tsai <thiago.tsai@gmail.com>
2016-08-13 09:10:28 +0100

    Reverse testing request location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @city = request.location.city
+    @city = request.location.city #Will return in english
   end
 end
