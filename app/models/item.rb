260e56d61eff5b324a6ba4d0c27b067e6982de11
Thiago Tsai <thiago.tsai@gmail.com>
2016-08-15 16:44:34 +0100

    Correct button pseudo classes

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ActiveRecord::Base
   belongs_to :user
   has_many :item_info, dependent: :destroy
   has_many :ingredients, through: :item_info
+  validates :name, length: { minimum: 3, maximum: 30 }
   mount_uploader :photo, PhotoUploader
   accepts_nested_attributes_for :ingredients
 end
