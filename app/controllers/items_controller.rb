01a196a5d3987df659a076581dca3d3ddaf803f1
Thiago Tsai <thiago.tsai@gmail.com>
2016-08-17 16:41:18 +0100

    Change item search controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,8 @@ class ItemsController < ApplicationController
   def search
     # Preparation to search view filters form
     default_city = request.location.city.blank? ? "Lisboa" : request.location.city
-    default_city = "Lisboa" if default_city == "Lisbon"
     @city = (params[:location].nil? or params[:location].blank?) ? default_city : params[:location]
+    @city = "Lisboa" if @city == "Lisbon"
     @sort = (params[:sort].nil? or params[:sort].blank?) ? 0 : params[:sort].to_i
     @cuisine_id = params[:cuisine_id].to_i
 
