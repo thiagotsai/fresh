c46a12c7512ef5f00c6697f45046b03f0dfde4ea
Thiago Tsai <thiago.tsai@gmail.com>
2019-06-09 21:33:06 +0100

    Fix hardcoded city

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,8 @@ class ItemsController < ApplicationController
 
   def search
     # Preparation to search view filters form
-    default_city = request.location.city.blank? ? "lisboa" : request.location.city
+    # default_city = request.location.city.blank? ? "lisboa" : request.location.city
+    default_city = "lisboa"
     @city = (params[:location].nil? or params[:location].blank?) ? default_city : params[:location].downcase
     @city = "lisboa" if @city == "lisbon"
     @sort = (params[:sort].nil? or params[:sort].blank?) ? 0 : params[:sort].to_i
